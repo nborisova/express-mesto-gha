6c8f6a264ed53586ff132b07537fedee427ba5e2
nborisova <borisova.natalia.14@gmail.com>
2023-08-11 02:52:47 +0300

    Базовая настройка серверной части

---
+++ models/cards.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: ObjectId,
+    required: true,
+  },
+  likes: { // список лайкнувших пост пользователей, массив ObjectId, по умолчанию — пустой массив (поле default)
+    type: Array,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
