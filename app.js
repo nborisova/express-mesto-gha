a07c8f86ddab38adda0a6f06fbfbfce0b9710105
nborisova <borisova.natalia.14@gmail.com>
2023-08-12 02:16:22 +0300

    настройка роутов пользователя

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const users = require('./models/users');
+const routerUsers = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
 });
 
 app.listen(PORT, () => {
   console.log(`Приложение слушает порт ${PORT}`);
 });
 
-app.get('/users', (req, res) => {
-  res.send(users);
-});
-
-app.get('/users/:userId', (req, res) => {
-  const { userId } = req.params;
-  res.send(userId);
-});
-
-app.post('/users', (req, res) => {
-
-});
+app.use('/users', routerUsers);
--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,44 @@
-const users = require('../models/users');
+const router = require('express').Router();
+const { users } = require('../models/users');
+
+const getAllUsers = (req, res, next) => {
+  res.send(users);
+
+  next();
+};
+
+const doesUserExist = (req, res, next) => {
+  const { userId } = req.params;
+
+  if (!users[userId]) {
+    res.send({ error: 'Такого пользователя нет' });
+    return;
+  }
+
+  next();
+};
+
+const sendUser = (req, res, next) => {
+  const { userId } = req.params;
+
+  res.send(users[userId]);
+
+  next();
+};
+
+const createUser = (req, res, next) => {
+  const { name, about, avatar } = req.body;
+
+  users.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+
+  next();
+};
+
+router.get('/', getAllUsers);
+router.get('/:userId', doesUserExist);
+router.get('/:userId', sendUser);
+router.post('/users', createUser);
+
+module.exports = router;
