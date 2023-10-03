eda9676855d55e32a080260afc01a16fc8d5f855
nborisova <borisova.natalia.14@gmail.com>
2023-10-03 23:46:18 +0300

    править localhost

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { errors } = require('celebrate');
 const commonRouter = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
