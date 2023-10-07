5cad1002697fd017bc725d08101d5320b79f18dd
nborisova <borisova.natalia.14@gmail.com>
2023-10-07 15:04:07 +0300

    объединение бэкенда и фронтенда

---
+++ frontend/src/utils/Api.js
@@ -0,0 +1,105 @@
+import React from "react";
+
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if(res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`,{
+      headers: this._headers
+    })
+    .then(this._checkResponse);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers
+    })
+    .then(this._checkResponse);
+  }
+
+  editProfile({ name, description }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        about: description
+      })
+    })
+    .then(this._checkResponse);
+  }
+
+  addNewCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        link: link,
+      })
+    })
+    .then(this._checkResponse);
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(this._checkResponse);
+  }
+
+  like(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: this._headers
+    })
+    .then(this._checkResponse);
+  }
+
+  dislike(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(this._checkResponse);
+  }
+
+  changeAvatar(avatarLink) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: avatarLink
+      })
+    })
+    .then(this._checkResponse);
+  }
+
+}
+
+export const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-66',
+  headers: {
+    authorization: 'd284ed82-9920-40c8-837a-2e288dd55f54',
+    'Content-Type': 'application/json'
+  }
+});
+
+export function checkResponse(res) {
+  if (res.ok) {
+      return res.json();
+  } 
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
